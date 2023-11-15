5db3af4b070711288d330d92eaa7cc4af005292b
Thurai Tun Za <147380435+thuraitunza@users.noreply.github.com>
2023-11-15 16:04:57 +0630

    Update nuxt.config.ts

--- nuxt_blog_app/nuxt.config.ts
+++ nuxt_blog_app/nuxt.config.ts
@@ -10,6 +10,16 @@ export default defineNuxtConfig({
   //     apiBase: process.env.BASE_URL
   //   }
   // },
+
+  runtimeConfig: {
+    // Private keys are only available on the server
+    apiSecret: '123',
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+    }
+  },
+  
   devtools: { enabled: true },
    modules: ['@nuxtjs/tailwindcss']
 
