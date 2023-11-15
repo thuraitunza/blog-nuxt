fb556d78b15821ee66384b1c6514f4a7f2c82f45
Thurai Tun Za <147380435+thuraitunza@users.noreply.github.com>
2023-11-15 16:13:19 +0630

    Update nuxt.config.ts

--- nuxt_blog_app/nuxt.config.ts
+++ nuxt_blog_app/nuxt.config.ts
@@ -24,3 +24,78 @@ export default defineNuxtConfig({
    modules: ['@nuxtjs/tailwindcss']
 
 })
+
+
+
+
+export default defineEventHandler(async (event) => {
+//     const config = useRuntimeConfig()
+//     const gMethod = getMethod(event);
+//     const paramsUrl = event.context.params._;
+//     let formData;
+//     if (gMethod === "POST") {
+//       formData = await readBody(event);
+//     }
+//     if (gMethod === "PUT") {
+//       formData = await readBody(event);
+//     }
+   
+//     let params = "";
+//     let split = "";
+//     if (event.req.url.includes("?")) {
+//       split = event.req.url.split("?");
+//       params = split[1];
+//     }
+//     let token = getCookie(event, "token");
+//     // console.log('token',token);
+//     const data = await $fetch(
+//       `${config.public.apiBase}/${paramsUrl}?${params}`,
+//       {
+//         method: gMethod ? gMethod : "GET",
+//         headers: {
+//           Accept: "application/json",
+//           "Content-Type": "application/json",
+//           // "X-API-TOKEN": config.apiSecret,
+//           Authorization: token,
+//         },
+//         body: formData ? formData : null,
+//       }
+//     );
+//     return { data };
+//    });
+
+   
+
+
+//  <script setup>
+// definePageMeta({
+//   layout: "custom",
+// });
+// const token = useCookie("token");
+// const form = ref({
+//   name: "",
+//   email: "",
+//   subject: "",
+//   message: "",
+//   token: token,
+// });
+// const isLoading = ref(false);
+// const errors = ref([]);
+// const router = useRouter();
+// const contact = async () => {
+//   const { data, pending, error, refresh } = await useFetch(
+//     `api/contacts/create`,
+//     {
+//       method: "POST",
+//       body: form.value,
+      
+//       onResponseError({ request, response, options }) {
+//         console.log('gg',response)
+//     // Handle the response errors
+//   }
+//     }
+    
+//   );
+// };
+// </script> 
+
